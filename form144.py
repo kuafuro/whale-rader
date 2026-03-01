b180882b10358eae6a3015e4377c0ecefa1265f8
kuafuro <mingcheng@kuafuorhk.com>
2026-03-01 22:47:29 +0800

    Extend time limit from 5 to 15 minutes

--- form144.py
+++ form144.py
@@ -36,7 +36,7 @@ headers = {'User-Agent': 'Form144Radar (pm_agent@example.com)'}
 url = 'https://www.sec.gov/cgi-bin/browse-edgar?action=getcurrent&type=144&owner=include&count=40&output=atom'
 
 now_utc = datetime.now(timezone.utc)
-time_limit = now_utc - timedelta(minutes=5)
+time_limit = now_utc - timedelta(minutes=15)
 
 response = requests.get(url, headers=headers)
 soup = BeautifulSoup(response.content, 'xml')
